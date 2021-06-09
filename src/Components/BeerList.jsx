c51f3ea39db7243e783663e884c5a9b68fe797fe
Joshua Sawyer <joshua@jcsawyer.me>
2021-06-09 23:57:34 +0100

    Create components + implement API route to populate table

--- src/Components/BeerList.jsx
+++ src/Components/BeerList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import BeerListItem from './BeerListItem';
 
 class BeerList extends Component {
 
@@ -7,25 +7,70 @@ class BeerList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: [],
+            beers: [],
         }    
     }
 
-    // Executes when the component is "mounted"
-    componentDidMount() {
-        this.fetchAPIData();
-    }
 
     // Get data from the API
-    fetchAPIData() {
-        // TODO: Add Axios Query
-    }
+    componentDidMount() {
+        fetch('https://api.punkapi.com/v2/beers').then((response) => {
+          return response.json();
+        }).then((data) => {
+          console.log(data);
+          this.setState({
+            beers: data
+          })
+        })
+      }
+   
+      render() {
 
-    render() {
+
+        // Create Instances for each Beer being rendered
+        const AllBeers = this.state.beers;
+        const BeerItems = AllBeers.map(function(beer) {
+            return <BeerListItem key={beer.id} beer={beer}/>
+        });
+
         return (
-            <div>
-                
+            <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-50">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                          Name
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                          ABV
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                          Volume
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                          Ingredients
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        {BeerItems}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
+          </div>
         );
     }
 }
--- src/Components/BeerListItem.jsx
+++ src/Components/BeerListItem.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
-export default function BeerListItem(props) {
+export default function BeerListItem({beer}) {
+
+    // console.log(beer.volume)
+
+    const beerVolume = beer.volume
+    const beerVolumeValue = beerVolume.value
+    const beerVolumeUnit = beerVolume.unit
+
+    // Handling Ingredients
+    // -- Combined malt and hop lists together.
+    const ingMalt = beer.ingredients.malt
+    const ingHop = beer.ingredients.hops
+    const combinedIng = ingHop.concat(ingMalt)
+
+    // -- The number of ingredients we want to display
+    const noOfIng = 3;
+    
+    // Create list of all ing names
+    let listOfIng = combinedIng.map(i => {
+        return i.name;
+    })
+
+    // Create a set of ingredients
+        // Remove Duplicates (Not the best way)
+    const setOfIng = new Set(listOfIng);
+    
+    listOfIng = [...setOfIng]; // Set back to list
+    listOfIng = listOfIng.slice(0, noOfIng); // Slice list to desired length
+
     return (
-        <div>
-            
-        </div>
+        <tr key={beer.id} className='bg-white'>
+        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{beer.name}</td>
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{beer.abv}</td>
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{beerVolumeValue} {beerVolumeUnit}</td>
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{listOfIng.join(', ')}</td>
+        </tr>  
     )
 }
