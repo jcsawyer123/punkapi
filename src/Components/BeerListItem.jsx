18cf83c13d0cd715bed87746e955cf3b258e7891
Joshua Sawyer <joshua@jcsawyer.me>
2021-06-10 04:34:15 +0100

    Add most of the rest of the application - Done over a period of a few hours with many distractions - One large squash commit (oops?)

--- src/Components/BeerListItem.jsx
+++ src/Components/BeerListItem.jsx
@@ -1,40 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react';
+import BeerInfo from './BeerInfo'
 
-export default function BeerListItem({beer}) {
+class BeerListItem extends Component {
 
-    // console.log(beer.volume)
 
-    const beerVolume = beer.volume
-    const beerVolumeValue = beerVolume.value
-    const beerVolumeUnit = beerVolume.unit
+    constructor(props) {
+        super(props);
+        this.state = {
+            key: props.id,
+            beer: props.beer
+        }    
+    }
 
-    // Handling Ingredients
-    // -- Combined malt and hop lists together.
-    const ingMalt = beer.ingredients.malt
-    const ingHop = beer.ingredients.hops
-    const combinedIng = ingHop.concat(ingMalt)
+    // Toggles Display of Modal
+    // ISSUE: When you close the modal you must double click to reopen it 
+    toggleModal() {
+        this.setState({
+            opened: !this.state.opened
+        })
+    }
 
-    // -- The number of ingredients we want to display
-    const noOfIng = 3;
+
+    render() {
     
-    // Create list of all ing names
-    let listOfIng = combinedIng.map(i => {
-        return i.name;
-    })
+        const beer = this.state.beer
 
-    // Create a set of ingredients
-        // Remove Duplicates (Not the best way)
-    const setOfIng = new Set(listOfIng);
-    
-    listOfIng = [...setOfIng]; // Set back to list
-    listOfIng = listOfIng.slice(0, noOfIng); // Slice list to desired length
+        const beerVolume = beer.volume
+        const beerVolumeValue = beerVolume.value
+        const beerVolumeUnit = beerVolume.unit
 
-    return (
-        <tr key={beer.id} className='bg-white'>
-        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{beer.name}</td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{beer.abv}</td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{beerVolumeValue} {beerVolumeUnit}</td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{listOfIng.join(', ')}</td>
-        </tr>  
-    )
+        // Handling Ingredients
+        // -- Combined malt and hop lists together.
+        const ingMalt = beer.ingredients.malt
+        const ingHop = beer.ingredients.hops
+        const combinedIng = ingHop.concat(ingMalt)
+
+        // -- The number of ingredients we want to display
+        const noOfIng = 3;
+        
+        // Create list of all ing names
+        let listOfIng = combinedIng.map(i => {
+            return i.name;
+        })
+
+        // Create a set of ingredients
+            // Remove Duplicates (Not the best way)
+        const setOfIng = new Set(listOfIng);
+        
+        listOfIng = [...setOfIng]; // Set back to list
+        
+        beer.ingStr = listOfIng.join(', ') // Store full list in seperate string
+
+        listOfIng = listOfIng.slice(0, noOfIng); // Slice list to desired length
+
+
+
+        return (
+            <tr key={beer.id} className='bg-gray-100 border-gray-10'>
+            <td onClick={this.toggleModal.bind(this)} className="clickable  px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 hover:underline">{beer.name}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{beer.abv}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{beerVolumeValue} {beerVolumeUnit}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{listOfIng.join(', ')}</td>
+            {this.state.opened && <BeerInfo toggleModal={this.toggleModal} beer={this.state.beer}/>}
+            </tr>    
+        )
+    }   
 }
+
+export default BeerListItem;
